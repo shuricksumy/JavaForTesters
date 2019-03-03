39f6e947df464b95179cbd5d60844e840ca91216
shurick <suchkov.av@gmail.com>
2019-03-03 17:26:14 +0200

    Task2 - Methods, Classes, Objects

---
+++ sandbox/src/main/java/com/shurick/trn/Point.java
@@ -0,0 +1,20 @@
+package com.shurick.trn;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class Point {
+
+  public double positionX;
+  public double positionY;
+
+  public Point (double positionX, double positionY){
+    this.positionX = positionX;
+    this.positionY = positionY;
+  }
+
+  public double getDistanceTo(Point point){
+    return sqrt(pow((point.positionX-this.positionX),2) + pow((point.positionY+this.positionY),2));
+  }
+
+}
