812ab06d9de449d1945b98a1505dfbcc43078984
shurick <suchkov.av@gmail.com>
2019-03-09 00:26:56 +0200

    Task 4.2 Add test for Contact Creation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,73 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String nickname;
+  private final String title;
+  private final String companyName;
+  private final String address;
+  private final String mobilePhone;
+  private final String emailAddress;
+  private final String homeSite;
+  private final DateData birthday;
+
+  public ContactData(String firstName, String middleName, String lastName, String nickname, String title, String companyName, String address, String mobilePhone, String emailAddress, String homeSite, DateData birthday) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickname = nickname;
+    this.title = title;
+    this.companyName = companyName;
+    this.address = address;
+    this.mobilePhone = mobilePhone;
+    this.emailAddress = emailAddress;
+    this.homeSite = homeSite;
+    this.birthday = birthday;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobilePhone() {
+    return mobilePhone;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public String getHomeSite() {
+    return homeSite;
+  }
+
+  public DateData getBirthday() {
+    return birthday;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
+
 import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTest {
@@ -15,27 +16,51 @@ public class GroupCreationTest {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php");
+    login("admin", "secret");
+  }
+
+  private void login(String user, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(user);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
 
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("TestGroup1");
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("TestGroup1Header");
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("TestGroup1Footer");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("groups")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("TestGroup1", "TestGroup1Header", "TestGroup1Footer"));
+    submitGroupForm();
+    gotoGroupPage();
+    refactor();
+  }
+
+  private void refactor() {
     wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitGroupForm() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
