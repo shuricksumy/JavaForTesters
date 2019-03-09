c0ef2857464030691e93e6caaebe132e845686d9
shurick <suchkov.av@gmail.com>
2019-03-09 18:09:42 +0200

    Task 5.2 Add new test - delete first group

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTest extends TestBase {
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("TestGroup1", "TestGroup1Header", "TestGroup1Footer"));
+    app.getGroupHelper().submitGroupForm();
+    app.getNavigationHelper().gotoGroupPage();
+  }
+
+}
