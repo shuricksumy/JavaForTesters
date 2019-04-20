63ccba2d5d8765c08a1cf0e97757f6cfa10ebe59
shurick <suchkov.av@gmail.com>
2019-04-20 22:51:53 +0300

    Task 17 Prepare new project

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,52 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
+import ru.stqa.pft.mantis.model.GlobalSettings;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+  private WebDriver wd;
+  private final Properties properties;
+  private String browser;
+  private GlobalSettings gs = new GlobalSettings();
+
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals(BrowserType.CHROME)) {
+      System.setProperty("webdriver.chrome.driver", properties.getProperty("chrome.driverPath"));
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.FIREFOX)) {
+      System.setProperty("webdriver.gecko.driver", properties.getProperty("firefox.driverPath"));
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.SAFARI)) {
+      wd = new SafariDriver();
+    }
+
+    wd.manage().timeouts().implicitlyWait(gs.getDefaultWaiterTime(), TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+    wd.manage().window().maximize();
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+
+}
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/GlobalSettings.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.mantis.model;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class GlobalSettings {
+
+  private final Properties properties;
+  private int defaultTimeout;
+  private int middleTimeout;
+  private int quickTimeout;
+  private String verifyUI;
+
+  public GlobalSettings() {
+    properties = new Properties();
+    String target = System.getProperty("target", "local");
+    try {
+      properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+      this.defaultTimeout = Integer.parseInt(properties.getProperty("waiter.defaultTime"));
+      this.middleTimeout = Integer.parseInt(properties.getProperty("waiter.middleTime"));
+      this.quickTimeout = Integer.parseInt(properties.getProperty("waiter.quickTime"));
+      this.verifyUI = properties.getProperty("test.verifyUI");
+    } catch (IOException ex) {
+      this.defaultTimeout = 30;
+      this.middleTimeout = 10;
+      this.quickTimeout = 1;
+      this.verifyUI = "true";
+    }
+  }
+
+  public int getDefaultWaiterTime() {
+    return defaultTimeout;
+  }
+
+  public int getMiddleWaiterTime() {
+    return middleTimeout;
+  }
+
+  public int getQuickWaiterTime() {
+    return quickTimeout;
+  }
+
+  public boolean isVerifyUI() {
+    return verifyUI.toLowerCase().equals("true");
+  }
+
+}
