c830df8dccf0b2411d5f3407fe02777fce546d2b
shurick <suchkov.av@gmail.com>
2019-04-23 01:44:27 +0300

    Adding logger

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -20,9 +20,11 @@ public class RegistrationTests extends TestBase {
 
   @Test
   public void testRegistration() throws IOException, MessagingException {
-    String email = "user5@localhost.localdomain";
-    String user = "user5";
+    long now = System.currentTimeMillis();
+    String email = String.format("user%s@localhost.localdomain", now);
+    String user = String.format("user%s", now);
     String password = "password";
+
     app.registration().start(user, email);
     List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
