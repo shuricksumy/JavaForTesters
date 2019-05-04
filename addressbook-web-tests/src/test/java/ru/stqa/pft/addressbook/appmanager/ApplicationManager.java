906f3a87ef5511d200087dd52e44e72ffca8edc9
shurick <suchkov.av@gmail.com>
2019-05-04 15:51:21 +0300

    Added remote web driver

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import ru.stqa.pft.addressbook.model.GlobalSettings;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -38,14 +42,21 @@ public class ApplicationManager {
     //Start before browser to save time in case db connection issue
     dbHelper = new DbHelper();
 
-    if (browser.equals(BrowserType.CHROME)) {
-      System.setProperty("webdriver.chrome.driver", properties.getProperty("chrome.driverPath"));
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.FIREFOX)) {
-      System.setProperty("webdriver.gecko.driver", properties.getProperty("firefox.driverPath"));
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.SAFARI)) {
-      wd = new SafariDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+
+      if (browser.equals(BrowserType.CHROME)) {
+        System.setProperty("webdriver.chrome.driver", properties.getProperty("chrome.driverPath"));
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.FIREFOX)) {
+        System.setProperty("webdriver.gecko.driver", properties.getProperty("firefox.driverPath"));
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.SAFARI)) {
+        wd = new SafariDriver();
+      }
+    }else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
     wd.manage().timeouts().implicitlyWait(gs.getDefaultWaiterTime(), TimeUnit.SECONDS);
